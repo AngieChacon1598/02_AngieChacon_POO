b693162cdead7e2c4bf631750e8fbe1b161246f4
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2024-03-11 22:14:17 -0500

    While

---
+++ 02_AngieChacon/src/S03_02/S03_02_While.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package S03_02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S03_02_While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+          int a = 1;
+
+        while (a <= 10) {
+
+            System.out.println("Hola");
+            a++;
+
+        }
+    }
+    
+}
