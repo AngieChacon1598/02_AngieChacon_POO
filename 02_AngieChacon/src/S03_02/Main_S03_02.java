d264b48e93c75eb5c9c599a42ae9a602fde7b06a
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2024-03-11 21:40:50 -0500

    version1

--- 02_AngieChacon/src/S03_02/Main_S03_02.java
+++ 02_AngieChacon/src/S03_02/Main_S03_02.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package S03_02;
 
 /**
@@ -11,8 +14,7 @@ public class Main_S03_02 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+        // TODO code application logic here
     }
     
 }
