9352245f6a49de76fa4f9e67a026d83c3cafdd60
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2024-03-11 22:17:50 -0500

    v.1

---
+++ 02_AngieChacon/src/S03_02/Main_S03_02.java
@@ -0,0 +1,17 @@
+
+package S03_02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Main_S03_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
