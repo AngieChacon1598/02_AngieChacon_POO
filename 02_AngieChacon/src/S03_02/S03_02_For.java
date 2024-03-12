afd5ca00029759cdf14a4b7dbfed954f17c9d1f4
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2024-03-11 22:10:17 -0500

    For

---
+++ 02_AngieChacon/src/S03_02/S03_02_For.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package S03_02;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class S03_02_For {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+         for (int i = 1; i <= 10; i++) {
+            System.out.println("Ejemplo numero " + i);
+
+        }
+    }
+    
+}
