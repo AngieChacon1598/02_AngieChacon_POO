c5abb1a73a16bfba20103d06286913621554d7b6
AngieChacon1598 <angie.chacon.a@vallegrande.edu.pe>
2024-03-11 22:09:26 -0500

    DoWhile

---
+++ 02_AngieChacon/src/S03_02/S03_02_DoWhile.java
@@ -0,0 +1,25 @@
+package S03_02;
+
+/**
+ *
+ * @author AngieChacon
+ */
+public class S03_02_DoWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //do = hacer
+        //while = mientras
+        int a = 100;
+
+        do {
+            System.out.println("Hola");
+
+        } while (a == 200);
+
+    }
+
+}
